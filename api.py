8fbf6c3fd0cd3b5efd1bd2d3b275a0356a0cee9e
ostergaardl-ELS <l.ostergaard@elsevier.com>
2021-10-13 12:20:05 -0400

    Added command line integration.

--- api.py
+++ api.py
@@ -2,73 +2,90 @@ import pandas as pd
 import numpy as np
 import requests
 
+import click
+
 from pandas.io.json import json_normalize
 import json
 
-pure_url = "https://<SITE>.pure.elsevier.com"
-api_key = "<API_KEY>"
-pure_api_url = "{}/ws/api/521/".format(pure_url)
-fields = "uuid,title.value,info.additionalExternalIds.*"
-
-def get_request(resource, params = {}):
+def get_request(pure_url, api_key, version, resource, params = {}):
 	headers = {
 		"accept": "application/json",
 		"api-key": api_key
 	}	
+
+	url = "{0}/ws/api/{1}/{2}".format(pure_url,version,resource)
+
 	try:
-		return requests.get(
-			"{}{}".format(pure_api_url, resource), 
-			params = params, 
-			headers = headers
-		)
+		return requests.get(url, params = params, headers = headers)
 	except requests.ConnectionError:
-		print("Request error! {0} ({1})".format(pure_api_url,params))
+		print("Request error! {0} ({1})".format(url,params))
 		return None	
 
-current = 0
-size = 50
-total = np.inf
+def fetch_data(url, api_key, version, fields = "uuid,title.value,info.additionalExternalIds.*"):
 
-dfs = []
+	current = 0
+	size = 50
+	total = np.inf
 
-while current < total:
+	dfs = []
 
-	pars = {
-		"fields": fields,
-		"size": size,
-		"offset": current
-	}
+	bar = click.progressbar(length=100, label="Extracting data:", show_eta=True)
 
-	dataset = get_request("research-outputs", pars).json()
+	while current < total:
+		pars = {
+			"fields": fields,
+			"size": size,
+			"offset": current
+		}
 
-	total = dataset['count'] 
-	current = current+size
-	print("{0}%".format( np.round(current / total * 100 , 2)))
+		dataset = get_request(url, api_key, version, "research-outputs", pars).json()
 
-	df_title = pd.json_normalize(data=dataset['items'])
+		total = dataset['count'] 
+		current = current + size
 
-	id_column = "info.additionalExternalIds"
+		df_title = pd.json_normalize(data=dataset['items'])
 
-	if id_column not in df_title.columns:
-		df[id_column] = None
-	isnull = df_title[id_column].isnull()
-	df_ids = pd.DataFrame()
-	for idx, row in df_title.loc[~isnull, :].iterrows():			
-		df_id = pd.json_normalize(row[id_column])
-		df_id['uuid'] = row['uuid']
-		df_id = df_id.set_index('uuid')
-		df_ids = pd.concat([df_ids, df_id])
+		id_column = "info.additionalExternalIds"
 
-	df_title = df_title.drop(id_column, axis=1).set_index('uuid')
+		if id_column not in df_title.columns:
+			df_title[id_column] = None
 
-	df = pd.pivot_table(df_title.join(df_ids), 
-		index=['uuid','title.value'], 
-		values=['value'], 
-		columns=['idSource'], 
-		aggfunc=';'.join)
+		isnull = df_title[id_column].isnull()
+		df_ids = pd.DataFrame()
+		for idx, row in df_title.loc[~isnull, :].iterrows():			
+			df_id = pd.json_normalize(row[id_column])
+			df_id['uuid'] = row['uuid']
+			df_id = df_id.set_index('uuid')
+			df_ids = pd.concat([df_ids, df_id])
 
-	dfs.append(df)
+		df_title = df_title.drop(id_column, axis=1).set_index('uuid')
 
+		df = pd.DataFrame()
+		if df_ids.empty:
+			df = df_title.copy()
+		else:
+			df = pd.pivot_table(df_title.join(df_ids), 
+				index=['uuid','title.value'], 
+				values=['value'], 
+				columns=['idSource'], 
+				aggfunc=';'.join)
 
-out_df = pd.concat(dfs)
-out_df.to_excel("ids.xlsx")
+		dfs.append(df)
+
+		progress = np.round(current/total,2)
+		bar.update(progress)
+
+	return pd.concat(dfs)
+
+@click.command()
+@click.argument("url")
+@click.argument("apikey")
+@click.option("--outputfile", help = "The name of the output Excel file (must include .xlsx extension).", default = "data.xlsx")
+@click.option("--apiversion", help = "The API version to use. Default: '521'", default = "521")
+def main(url, apikey, outputfile, apiversion):
+
+	click.echo("Connecting to {}.".format(url))
+	data = fetch_data(url, apikey, apiversion)
+	data.to_excel(outputfile)
+
+main()
